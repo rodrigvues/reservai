7310aa70ae6f662cb5ff856a928661b3d092aa5d
Vitor Rodrigues <vitor.rodriguess3112@gmail.com>
2025-06-16 21:49:44 -0300

    basic routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./screens/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./screens/login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path: 'profile',
+    loadComponent: () =>
+      import('./screens/profile/profile.component').then(m => m.ProfileComponent)
+  },
+];
